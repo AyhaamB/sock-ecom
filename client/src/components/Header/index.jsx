e2a83840bdd66288def8c473b27706efd82667e8
AyhaamB <ayh132@gmail.com>
2024-01-31 00:37:13 -0500

    setting up files

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -11,12 +11,6 @@ const Header = () => {
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
-          </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
-        </div>
-        <div>
           {Auth.loggedIn() ? (
             <>
               <Link className="btn btn-lg btn-info m-2" to="/me">
